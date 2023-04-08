5deabd0269a636c1e2d557f67c5aaf0a17353030
Jonay <alu0101100586@ull.edu.es>
2023-04-08 11:34:31 +0100

    feat(backend): first version of the user system

---
+++ Back-End/middlewares/authMiddleware.js
@@ -0,0 +1,31 @@
+const jwt = require('../utils/jwt');
+
+function asureAuthenticated(req, res, next) {
+    const token = req.headers.authorization;
+    
+    if (!token) {
+      return res.status(403)
+        .send({ msg: 'Cabecera de Autorización no otorgada' });
+    }
+
+    const tokenParts = token.replace('Bearer ', '');
+
+    try {
+      const payload = jwt.decodeToken(tokenParts);
+      const { exp } = payload;
+      const now = new Date().getTime();
+
+      if (exp <= now) {
+        return res.status(403).send({ msg: 'Token expirado' });
+      }
+
+      req.user = payload;
+      next();
+    } catch (error) {
+      return res.status(403).send({ msg: 'Token no válido' });
+    }
+}
+
+module.exports = {
+  asureAuthenticated,
+};
