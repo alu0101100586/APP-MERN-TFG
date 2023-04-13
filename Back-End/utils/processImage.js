c7a3c9787b74245a9c8e44ac8004e4905afa6977
Jonay <alu0101100586@ull.edu.es>
2023-04-13 18:47:12 +0100

    refactor: add avatar to user and finished user api

---
+++ Back-End/utils/processImage.js
@@ -0,0 +1,11 @@
+function getFilePath(file) {
+  const filePath = file.path;
+  const fixFilePath = filePath.replace(/\\/g, '/');
+  const fileSplit = fixFilePath.split('/');
+  const fileName = fileSplit[1] + '/' + fileSplit[2];
+  return fileName;
+}
+
+module.exports = {
+  getFilePath,
+};
