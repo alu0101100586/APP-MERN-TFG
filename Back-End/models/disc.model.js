5528666a14cdc1677b79ebc7153ebffe1773f1c1
Jonay <alu0101100586@ull.edu.es>
2023-04-17 17:51:23 +0100

    feat(backend): disc system finished

---
+++ Back-End/models/disc.model.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate");
+const musicGenreEnum = require("../enum/musicGenre.enum");
+
+const DiscSchema = mongoose.Schema({
+  ownerId: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  releaseDate: {
+    type: Date,
+    required: true,
+  },
+  moneyLimit: {
+    type: Number,
+    required: true,
+  },
+  raisedMoney: {
+    type: Number,
+    default: 0,
+    required: true,
+  },
+  musicalGenre: [{
+    type: String,
+    enum: Object.values(musicGenreEnum),
+    required: true,
+    }],
+  cover: { type: String },
+  songs: [{ 
+    type: String, 
+    required: true, 
+    trim: true,
+  }],
+}, { versionKey: false,
+     timestamps: true });
+
+DiscSchema.plugin(mongoosePaginate);
+
+module.exports = mongoose.model("Disc", DiscSchema);
